5d7ad3f743027e9a7ae6a3d23adf31c4ca4b194a
Nate Spilman <nate.spilman@gmail.com>
2024-01-20 23:00:19 -0800

    As a User, I can provide my email and access the music streaming page (#15)
    * Allowing users to provide their email and access the protected page
    
    * removal on unused var

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -8,6 +8,7 @@ export async function GET(request: Request) {
   // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-sign-in-with-code-exchange
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
+  const redirect = requestUrl.searchParams.get("redirect");
 
   if (code) {
     const cookieStore = cookies();
@@ -16,5 +17,5 @@ export async function GET(request: Request) {
   }
 
   // URL to redirect to after sign in process completes
-  return NextResponse.redirect(requestUrl.origin);
+  return NextResponse.redirect(requestUrl.origin + (redirect || ""));
 }
