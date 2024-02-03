92d1c7741f65c101832d94204fa62030ddcdd613
Nate Spilman <nate.spilman@gmail.com>
2024-02-02 21:39:36 -0800

    auth callback update

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -30,5 +30,7 @@ export async function GET(request: Request) {
   }
 
   // URL to redirect to after sign in process completes
-  return NextResponse.redirect(requestUrl.origin + (redirect || ""));
+  return NextResponse.redirect(
+    process.env.NEXT_PUBLIC_BASE_URL + (redirect || "")
+  );
 }
