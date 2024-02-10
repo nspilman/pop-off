d6dee79f5cfc0934171ba696118d46a59132dbbb
Nate Spilman <nate.spilman@gmail.com>
2024-02-09 22:49:15 -0800

    debugging auth

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -2,6 +2,7 @@ import { createClient } from "@/utils/supabase/server";
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 import { decodeToken } from "@/utils/generateShareLink";
+import { TOAST_REDIRECT_KEY } from "@/constants";
 
 export async function GET(request: Request) {
   // The `/auth/callback` route is required for the server-side auth flow implemented
@@ -11,25 +12,28 @@ export async function GET(request: Request) {
   const code = requestUrl.searchParams.get("code");
   const redirect = requestUrl.searchParams.get("redirect");
   const referral = requestUrl.searchParams.get("referral");
-  if (code) {
-    const cookieStore = cookies();
-    const supabase = createClient(cookieStore);
-    const {
-      data: { user },
-    } = await supabase.auth.exchangeCodeForSession(code);
-    if (referral) {
-      const referringUserId = decodeToken(referral);
-      if (user?.id && referringUserId !== user.id) {
-        const { data, error } = await supabase.from("referrals").insert({
-          referring_user_id: referringUserId,
-          referred_user: user.id,
-        });
+  try {
+    if (code) {
+      const cookieStore = cookies();
+      const supabase = createClient(cookieStore);
+      const {
+        data: { user },
+      } = await supabase.auth.exchangeCodeForSession(code);
+      if (referral) {
+        const referringUserId = decodeToken(referral);
+        if (user?.id && referringUserId !== user.id) {
+          const { data, error } = await supabase.from("referrals").insert({
+            referring_user_id: referringUserId,
+            referred_user: user.id,
+          });
+        }
       }
     }
-  }
 
-  // URL to redirect to after sign in process completes
-  return NextResponse.redirect(
-    process.env.NEXT_PUBLIC_BASE_URL + (redirect || "")
-  );
+    const successRedirect = process.env.NEXT_PUBLIC_BASE_URL + (redirect || "");
+    // URL to redirect to after sign in process completes
+    return NextResponse.redirect(successRedirect);
+  } catch (e: any) {
+    NextResponse.redirect(`/?${TOAST_REDIRECT_KEY}=${e.message}`);
+  }
 }
