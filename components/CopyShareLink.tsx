5ffbcd7456e56415e9441561d4b9931a13c642ff
Nate Spilman <nate.spilman@gmail.com>
2024-02-10 23:18:15 -0800

    toast upates

--- components/CopyShareLink.tsx
+++ components/CopyShareLink.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "./ui/button";
+import { useToast } from "./ui/use-toast";
 
 interface Props {
   link: string;
@@ -11,7 +12,7 @@ export const CopyShareLink = ({ link }: Props) => {
     try {
       await navigator.clipboard.writeText(endpoint);
       alert(
-        "Your unique share link is copied to your clipboard. When your friends access the site using your link, you'll get credit for the referral! The reward for now will be deep displays of us, but who knows, maybe we'll get stickers."
+        "Copied to clipboard! Your unique share link is copied to your clipboard. When your friends access the site using your link, you'll get credit for the referral! The reward for now will be deep displays of us, but who knows, maybe we'll get stickers."
       );
     } catch (err) {
       console.error("Failed to copy: ", err);
