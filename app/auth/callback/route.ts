f14f489fce6504cc8f665f4aa2c71c86d01da57e
Nate Spilman <nate.spilman@gmail.com>
2024-02-16 09:36:14 -0800

    changing auth to SSR to handle when the gmail client doesn't send the cookie in the header

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,39 +1,53 @@
-import { createClient } from "@/utils/supabase/server";
-import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+import { type CookieOptions, createServerClient } from "@supabase/ssr";
 import { decodeToken } from "@/utils/generateShareLink";
 import { TOAST_REDIRECT_KEY } from "@/constants";
 
 export async function GET(request: Request) {
-  // The `/auth/callback` route is required for the server-side auth flow implemented
-  // by the Auth Helpers package. It exchanges an auth code for the user's session.
-  // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-sign-in-with-code-exchange
-  try {
-    const requestUrl = new URL(request.url);
-    const code = requestUrl.searchParams.get("code");
-    const redirect = requestUrl.searchParams.get("redirect");
-    const referral = requestUrl.searchParams.get("referral");
-    if (code) {
-      const cookieStore = cookies();
-      const supabase = createClient(cookieStore);
-      const {
-        data: { user },
-      } = await supabase.auth.exchangeCodeForSession(code);
-      if (referral) {
-        const referringUserId = decodeToken(referral);
-        if (user?.id && referringUserId !== user.id) {
-          const { data, error } = await supabase.from("referrals").insert({
-            referring_user_id: referringUserId,
-            referred_user: user.id,
-          });
-        }
+  const { searchParams, origin } = new URL(request.url);
+  const code = searchParams.get("code");
+  // if "next" is in param, use it as the redirect URL
+  const redirect = searchParams.get("redirect") ?? "/";
+  const referral = searchParams.get("referral");
+
+  if (code) {
+    const cookieStore = cookies();
+    const supabase = createServerClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      {
+        cookies: {
+          get(name: string) {
+            return cookieStore.get(name)?.value;
+          },
+          set(name: string, value: string, options: CookieOptions) {
+            cookieStore.set({ name, value, ...options });
+          },
+          remove(name: string, options: CookieOptions) {
+            cookieStore.delete({ name, ...options });
+          },
+        },
+      }
+    );
+    const {
+      error,
+      data: { user },
+    } = await supabase.auth.exchangeCodeForSession(code);
+    if (referral) {
+      const referringUserId = decodeToken(referral);
+      if (user?.id && referringUserId !== user.id) {
+        const { data, error } = await supabase.from("referrals").insert({
+          referring_user_id: referringUserId,
+          referred_user: user.id,
+        });
       }
     }
-
-    const successRedirect = process.env.NEXT_PUBLIC_BASE_URL + (redirect || "");
-    // URL to redirect to after sign in process completes
-    return NextResponse.redirect(successRedirect);
-  } catch (e: any) {
-    NextResponse.redirect(`/?${TOAST_REDIRECT_KEY}=${e.message}`);
+    if (!error) {
+      return NextResponse.redirect(`${origin}${redirect}`);
+    }
+    NextResponse.redirect(`/?${TOAST_REDIRECT_KEY}=${error?.message}`);
   }
+  NextResponse.redirect(`/?${TOAST_REDIRECT_KEY}=${"Invalid login token"}`);
+  // return the user to an error page with instructions
 }
