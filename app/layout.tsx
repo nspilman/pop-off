6f0e7eead0f1eb340322be926656ff8a52f4b9c1
Nate Spilman <nate.spilman@gmail.com>
2024-02-18 09:39:11 -0800

    updating metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,23 @@ export const metadata = {
   favicon: "./favicon.ico",
   description:
     "Listen to Falling by Toneway before the song hits the major streaming platforms",
+  og: {
+    title:
+      "Toneway | Seattle-based Pop Group | Listen to Falling before it's released to the public",
+    description:
+      "Listen to Falling by Toneway before the song hits the major streaming platforms",
+    image:
+      "https://pgxxxhjpdbarogibubuk.supabase.co/storage/v1/object/public/Toneway/cover%20art/emailBg.jpeg?t=2024-02-18T17%3A35%3A35.217Z",
+    url: defaultUrl,
+    type: "music.song",
+    site_name: "Toneway Music",
+  },
+  canonical: defaultUrl,
+  robots: "index, follow",
+  viewport: "width=device-width, initial-scale=1",
+  charset: "UTF-8",
+  lang: "en",
+  themeColor: "#000000",
 };
 
 export default function RootLayout({
